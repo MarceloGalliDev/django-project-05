295c64e6bbb85bd117a449dd0705b1793473e2f7
Marcelo Galli <marcelolemesgalli@hotmail.com>
2023-11-25 20:02:15 -0300

    update
    update

--- produtos/models.py
+++ produtos/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 
 class ProdutosModels(models.Model):
+    codigo = models.IntegerField(primary_key=True, verbose_name="Código")
     nome = models.CharField(max_length=255, blank=False, null=False, verbose_name="Nome")
     descricao_curta = models.TextField(max_length=255, blank=False, null=False, verbose_name="Descrição curta")
     descricao_longa = models.TextField(max_length=255, blank=False, null=False, verbose_name="Descrição longa")
@@ -25,7 +26,16 @@ class ProdutosModels(models.Model):
         ),
         verbose_name="Tipo"
     )
-
+    
+    def __str__(self):
+        return self.nome
+    
+    class Meta:
+        verbose_name = "Produto"
+        verbose_name_plural = "Produtos"
+        indexes = [
+            models.Index(fields=["codigo"], name="codigo_produto_idx"),
+        ]
 
 """
 Produto:
