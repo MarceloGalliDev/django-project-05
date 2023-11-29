d41571414d7bf56c65e7d4e6d16985c37997a9bc
Marcelo Galli <marcelolemesgalli@hotmail.com>
2023-11-29 00:00:39 -0300

    update
    update

--- perfils/models.py
+++ perfils/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+
+class PerfilModels(models.Model):
+    ESTADOS_CHOICES = [
+        ('AC', 'Acre'),
+        ('AL', 'Alagoas'),
+        ('AP', 'Amapá'),
+        ('AM', 'Amazonas'),
+        ('BA', 'Bahia'),
+        ('CE', 'Ceará'),
+        ('DF', 'Distrito Federal'),
+        ('ES', 'Espírito Santo'),
+        ('GO', 'Goiás'),
+        ('MA', 'Maranhão'),
+        ('MT', 'Mato Grosso'),
+        ('MS', 'Mato Grosso do Sul'),
+        ('MG', 'Minas Gerais'),
+        ('PA', 'Pará'),
+        ('PB', 'Paraíba'),
+        ('PR', 'Paraná'),
+        ('PE', 'Pernambuco'),
+        ('PI', 'Piauí'),
+        ('RJ', 'Rio de Janeiro'),
+        ('RN', 'Rio Grande do Norte'),
+        ('RS', 'Rio Grande do Sul'),
+        ('RO', 'Rondônia'),
+        ('RR', 'Roraima'),
+        ('SC', 'Santa Catarina'),
+        ('SP', 'São Paulo'),
+        ('SE', 'Sergipe'),
+        ('TO', 'Tocantins'),        
+    ]
+
+    usuario = models.OneToOneField()
+"""
+PerfilUsuario
+    user - FK user (ou OneToOne)
+    idade - Int
+    data_nascimento - Date
+    cpf - char
+    endereco - char
+    numero - char
+    complemento - char
+    bairro - char
+    cep - Char
+    cidade - char
+    estado - Choices
+"""
