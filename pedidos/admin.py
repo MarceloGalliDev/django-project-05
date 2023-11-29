7a7aa5b45405694e70f1b42a95515d498880b7f6
Marcelo Galli <marcelolemesgalli@hotmail.com>
2023-11-28 22:23:35 -0300

    update
    update

--- pedidos/admin.py
+++ pedidos/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import PedidosModels, ItensPedidosModels
 
-# Register your models here.
+
+class ItensPedidosInline(admin.TabularInline):
+    model = ItensPedidosModels
+    extra = 1
+
+
+class PedidosInlineAdmin(admin.ModelAdmin):
+    inlines = [
+        ItensPedidosInline
+    ]
+
+
+admin.site.register(PedidosModels, PedidosInlineAdmin)
+admin.site.register(ItensPedidosModels)
